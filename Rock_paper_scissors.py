535663e1b2dfda46f90e81f5013ba355a6af5515
Epicinventor <166964367+Epicinventor@users.noreply.github.com>
2024-12-31 11:09:46 -0800

    Update Rock_paper_scissors.py

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -1,57 +1,23 @@
-### importing random ###
-
 import random
 
+def play():
+    choices = ['rock', 'paper', 'scissors']
+    user_choice = input("Enter rock, paper, or scissors: ").lower()
+    if user_choice not in choices:
+        print("Invalid choice!")
+        return
 
-### Rock paper scissors battle introduction
+    computer_choice = random.choice(choices)
+    print(f"Computer chose: {computer_choice}")
 
-print("Welcome to Rock-Paper-Scissors\n")
-print("Choose your material\n")
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+         (user_choice == 'paper' and computer_choice == 'rock') or \
+         (user_choice == 'scissors' and computer_choice == 'paper'):
+        print("You win!")
+    else:
+        print("You lose!")
 
-
-### Choosing the Material ###
-print("For Rock type letter 'r' \n")
-print("For Rock ")
-option = input("Choose > ").lower()
-
-### bot function ###
-list = [1,2,3]
-
-
-print(random.choice(list))
-
-print()
-print()
-print()
-print()
-
-### Code for Bot and Player using same ###
-if list == 1 and option == "r":
-    print("Draw!")
-
-elif list == 2 and option == "p":
-    print("Draw!")
-
-elif list == 3 and option == "s":
-    print("Draw!")
-
-### Code for Bot using Rock ###
-elif list == 1 and option == "p":
-    print("You Win!")
-
-elif list == 1 and option == "s":
-    print("Bot Wins!")
-
-### Code for Both using Paper ###
-elif list == 2 and option == "r":
-    print("Bot Wins!")
-
-elif list == 2 and option == "s":
-    print("You Win!")
-
-### Code for Bot using Scissors ###
-elif list == 3 and option == "r":
-    print("You Win!")
-
-elif list == 3 and option == "p":
-    print("Bot Wins!")
+if __name__ == "__main__":
+    play()
